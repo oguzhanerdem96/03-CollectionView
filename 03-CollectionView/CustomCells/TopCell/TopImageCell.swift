882cc547f175acbabaa5966ba9c31a32a09e72d9
oguzhanerdem96 <oguzhan_erdem96@hotmail.com>
2022-04-09 04:56:36 +0300

    Finished

--- 03-CollectionView/CustomCells/TopCell/TopImageCell.swift
+++ 03-CollectionView/CustomCells/TopCell/TopImageCell.swift
@@ -10,4 +10,97 @@ import UIKit
 
 class TopImageCell: UICollectionViewCell {
     static var identifier = "TopImagesCell"
+    
+    var myImages: [String]? {
+        didSet {
+            myCollectionView.reloadData()
+        }
+    }
+    
+    private let myCollectionView: UICollectionView = {
+           let layout = UICollectionViewFlowLayout()
+           layout.scrollDirection = .horizontal
+           let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+           
+           cv.backgroundColor = .clear
+           cv.showsHorizontalScrollIndicator = false
+           // register iconCell
+           cv.register(IconCell.self,
+                       forCellWithReuseIdentifier: IconCell.identifier)
+            cv.translatesAutoresizingMaskIntoConstraints = false
+           return cv
+       }()
+     override init(frame: CGRect) {
+           super.init(frame: frame)
+           setupViews()
+       }
+       required init?(coder: NSCoder) {
+           fatalError("qweqw")
+       }
+      func setupViews() {
+           contentView.addSubview(myCollectionView)
+           
+           setCollectionViewConstraints()
+           myCollectionView.delegate = self
+           myCollectionView.dataSource = self
+       }
+       
+   }
+
+  //MARK: -  Delegate, DataSource
+   extension TopImageCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+       
+       
+       // Kaç tane hücre
+       func collectionView(_ collectionView: UICollectionView,
+                           numberOfItemsInSection section: Int) -> Int {
+           return MainVC.bottomImageArr.count
+       }
+       // Cellerin datası
+       func collectionView(_ collectionView: UICollectionView,
+                           cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+           
+           let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: IconCell.identifier, for: indexPath) as! IconCell
+           
+          
+           
+           
+           let myElement = MainVC.albumArr[indexPath.row]
+         
+               cell.imageView.image = UIImage(named: myElement)
+           
+//
+//           cell.imageView.image = UIImage(named: "a3")
+           
+           return cell
+       }
+       // Her bir item w,h
+       func collectionView(_ collectionView: UICollectionView,
+                           layout collectionViewLayout: UICollectionViewLayout,
+                           sizeForItemAt indexPath: IndexPath) -> CGSize {
+           
+           return CGSize(width: 200, height: contentView.frame.height - 10)
+       }
+       // Kenar boşluk
+       func collectionView(_ collectionView: UICollectionView,
+                           layout collectionViewLayout: UICollectionViewLayout,
+                           insetForSectionAt section: Int) -> UIEdgeInsets {
+           return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+       }
+
+   }
+ 
+
+//MARK: -  Constraints
+extension  TopImageCell {
+    
+    private func setCollectionViewConstraints(){
+        NSLayoutConstraint.activate([
+            myCollectionView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            myCollectionView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            myCollectionView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            myCollectionView.bottomAnchor.constraint(equalTo:  contentView.bottomAnchor)
+        
+        ])
+    }
 }
--- 03-CollectionView/MainVC.swift
+++ 03-CollectionView/MainVC.swift
@@ -12,19 +12,31 @@ class MainVC: UIViewController {
     static var bottomImageArr = ["a5","a3","v1","v2","v3", "v4"]
         static var albumArr = ["v1","v2","a3","a4","a5","a6","a7","a8","v3","v1","a2","v3", "v4"]
     
+    // background Image , All views
+    private let myView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "bl2")
+        iv.contentMode = .scaleToFill
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+
+    
+    
+    
+    
     private let myCollectionView: UICollectionView = {
         //layout değişkenine UICollectionViewFlowLayout diyerek "layout" değişkenimizi UICollectionViewFlowLayout komutlarını etkinleştiriyoruz.
         let layout = UICollectionViewFlowLayout()
         // collectionViewimize "frame" zero diyerek çerçeveyi sıfırlayıp collectionViewLayoutumuzun ise  verdiğimiz "layout" değişkeni olduğunu belirtiyoruz.
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
        // collectionViewe kendi sınırlarımız içinde olacağını belirtiyoruz.
-        cv.translatesAutoresizingMaskIntoConstraints = false
-        layout.scrollDirection = .vertical
-        layout.minimumLineSpacing = 5
         
+        layout.scrollDirection = .vertical
+        cv.backgroundColor = .clear
         cv.register(TopImageCell.self, forCellWithReuseIdentifier: TopImageCell.identifier)
         cv.register(BottomCell.self, forCellWithReuseIdentifier: BottomCell.identifier)
-        
+        cv.translatesAutoresizingMaskIntoConstraints = false
         
       return cv
     }()
@@ -33,11 +45,17 @@ class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        
+
+        
+    }
+    func setupViews() {
+        view.addSubview(myView)
         view.addSubview(myCollectionView)
         setCollectionViewConstraints()
         myCollectionView.delegate = self
         myCollectionView.dataSource = self
-
         
     }
 
@@ -72,19 +90,44 @@ extension MainVC: UICollectionViewDelegate , UICollectionViewDataSource {
     }
             func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
                 
+                print("mysection", indexPath.section)
                 // bttmCell section
                 if indexPath.section == 1 {
                     let bttmCell = myCollectionView.dequeueReusableCell(withReuseIdentifier: BottomCell.identifier, for: indexPath) as! BottomCell
-                    bttmCell.backgroundColor = .blue
+                    bttmCell.bottomImages = MainVC.albumArr[indexPath.item]
                     return bttmCell
                     
                 }
                 // top section
                 let topCell = myCollectionView.dequeueReusableCell(withReuseIdentifier: TopImageCell.identifier, for: indexPath) as! TopImageCell
-                topCell.backgroundColor = .green
+                topCell.myImages = MainVC.bottomImageArr
                 
                 return topCell
             }
+    
+    
+}
+extension MainVC: UICollectionViewDelegateFlowLayout {
+    // her hücre boyutu width , height
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        // section 1 için
+        if indexPath.section == 1 {
+            return CGSize(width: (view.frame.width/3) - 16, height: 100)
+        }
+        // üst section için
+        return CGSize(width: view.frame.width, height: 400)
+    
+    }
+    // hücreler arası boşluk için
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if section == 1 {
+            return UIEdgeInsets(top: 10, left: 8, bottom: 10, right: 8)
+        }
+        return UIEdgeInsets(top: 0, left: 0, bottom: 10, right: 0)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
     
     
 }
